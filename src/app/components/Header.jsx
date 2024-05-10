44211a3e450f687d59cc9928872e62c43882d868
Casper Banemann <casperbanemann@gmail.com>
2024-05-10 17:36:45 +0200

    folderfix

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from './Header.module.css';
+
+const Header = () => {
+  return (
+    <main className={styles.header}>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.link}>
+            <Link prefetch={false} href="/">
+              <Image src="/diversa.svg" alt="Logo" className={styles.logo} width={100} height={100} priority />
+            </Link>
+          </li>
+          <li className={styles.link}>
+            <Link prefetch={false} className={styles.link} href="/rules">
+              Rules
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
+};
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import Header from './components/Header';
+import styles from './styles.module.css'; 
+
+export const metadata = {
+  title: "Home",
+  description: "Home page",
+}
+
+export default function Home() {
+  return (
+    <main className={styles.fullHeight}>
+      <Header/>
+
+      
+
+      <div className={styles.contentContainer}>
+        <div className={styles.infoContainer}>
+        <h1 className={styles.title}>Accessibility Checker</h1>
+        <p className={styles.description}>
+          Enter a URL to check the websites accessibility
+        </p>
+      </div>
+
+        <form action="/report" className={styles.formContainer}>
+          <div className={styles.inputGroup}>
+            <label className={styles.formLabel} htmlFor="url"/>
+            <input className={styles.inputField} type="text" id="url" name="url" placeholder="Enter URL" required></input>
+          </div>
+          <button className={styles.submitButton} type="submit">
+            Check it
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+}
